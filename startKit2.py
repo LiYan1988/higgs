cff30ccbc9ae1c812ea2f087b3c6911702341fdd
Li Yan <li.yan.ly414@gmail.com>
2016-10-09 23:53:40 +0200

    Ensemble CV

--- startKit2.py
+++ startKit2.py
@@ -8,8 +8,9 @@ Created on Fri Sep 23 13:26:08 2016
 import pandas as pd
 import numpy as np
 import math
+import copy
 from sklearn import (preprocessing, model_selection, naive_bayes,
-                     linear_model, ensemble)
+                     linear_model, ensemble, metrics)
 
 def load_data():
     x_train = pd.read_csv('../train.csv')
@@ -58,11 +59,12 @@ def data_processing(x_train, x_test):
     """Process data
     """
     X = pd.concat([x_train, x_test], ignore_index=True)
+    X.replace(to_replace=-999, value=np.nan)
     cols = list(X.columns)
     for i, c in enumerate(cols):
-        xc = X[c].copy()
+        xc = X[c]
         xc_ave = np.mean(xc[xc!=-999])
-        X[c][xc==-999] = xc_ave
+        X[c].fillna(value=xc_ave, inplace=True)
 
     X_scale = preprocessing.scale(X)
     X = pd.DataFrame(data=X_scale, columns=cols)
@@ -71,6 +73,84 @@ def data_processing(x_train, x_test):
     x_test = X.iloc[x_train.shape[0]:,:]
 
     return x_train, x_test, X
+    
+def cv_predict_proba(clf, X, y, cv=3, random_state=0):
+    """predict probability based on CV
+    """
+    kf = model_selection.StratifiedKFold(n_splits=cv, shuffle=True,
+        random_state=random_state)
+    ypred = np.zeros((X.shape[0], len(np.unique(y))))
+    for train_index, test_index in kf.split(X, y):
+        clf.fit(X.ix[train_index], y[train_index])
+        ypred[test_index] = clf.predict_proba(X.ix[test_index])
+        
+    return ypred
+    
+def model_ensemble(models, Xtrain, ytrain, Xtest, cv=3, random_state=0, 
+                   model_g=None):
+    """Ensemble multiple models
+    models: list of models 
+    model_g: model used to generalize, can be simple average, or linear model
+    Xtrain: training set
+    ytrain: labels
+    Xtest: testing set
+    cv: cv folds
+    random_state: can be vector
+    """
+    x_train_pred = np.zeros((Xtrain.shape[0], len(models)))
+    x_test_pred = np.zeros((Xtest.shape[0], len(models)))
+    scores = np.zeros((len(models), 1))
+    for i, model in enumerate(models):
+        if type(random_state)==int:
+            rs = random_state
+        else:
+            rs = random_state[i]
+        x_train_pred[:, i] = cv_predict_proba(model, Xtrain, ytrain, 
+            cv, rs)[:, 1]
+        model.fit(Xtrain, ytrain)
+        x_test_pred[:, i] = model.predict_proba(Xtest)[:, 1]
+        scores[i] = metrics.roc_auc_score(ytrain, x_train_pred[:, i])
+        
+    if model_g is not None:
+        model_g.fit(x_train_pred, ytrain)
+        ypred = model_g.predict(x_test_pred) # or predict_proba?
+    else:
+        ypred = np.mean(x_test_pred)
+    
+    return ypred, scores
+    
+def model_ensemble_cv(models, Xtrain, ytrain, Xtest, cv=3, random_state=0, 
+                   model_g=None):
+    """
+    """
+    x_train_pred = np.zeros((Xtrain.shape[0], len(models)))
+    x_test_pred = np.zeros((Xtest.shape[0], len(models)*cv))
+    scores = np.zeros((len(models), cv))
+    for i, model in enumerate(models):
+        if type(random_state)==int:
+            rs = random_state
+        else:
+            rs = random_state[i]
+        kf = model_selection.StratifiedKFold(n_splits=cv, shuffle=True,
+            random_state=rs)
+        k = 0
+        for train_index, test_index in kf.split(Xtrain, ytrain):
+            model.fit(Xtrain.iloc[train_index,:], ytrain.iloc[train_index])
+            x_train_pred[test_index, i] = \
+                model.predict_proba(Xtrain.iloc[test_index, :])[:, 1]
+            x_test_pred[:, (i-1)*cv+k] = \
+                model.predict_proba(Xtest)[:, 1]
+            scores[i, k] = metrics.roc_auc_score(ytrain.iloc[test_index], 
+                x_train_pred[test_index, i])
+            k = k+1
+        
+    if model_g is not None:
+        model_g.fit(x_train_pred, ytrain)
+        ypred = model_g.predict(x_test_pred) # or predict_proba?
+    else:
+        ypred = np.mean(x_test_pred, axis=1)
+    
+    return ypred, scores, x_test_pred, x_train_pred
     
 if __name__ == '__main__':
     np.random.seed(0)
@@ -102,15 +182,29 @@ if __name__ == '__main__':
 #    y_pred = np.random.randint(2, size=y_train_test.shape)
 #    ams = calc_ams(y_pred, y_train_test, weight_test)
     
-    model = naive_bayes.GaussianNB()
-    model = linear_model.LogisticRegression()
-    model = ensemble.GradientBoostingClassifier(max_depth=9, verbose=10)
-    model = ensemble.ExtraTreesClassifier(n_estimators=300, random_state=0,
-        n_jobs=7, verbose=10)
-    model = ensemble.RandomForestClassifier(n_estimators=500, n_jobs=8, 
-        verbose=10)
-    model.fit(x_train, y_train)
-    y_pred = model.predict(x_test)
-    y_pred_proba = model.predict_proba(x_test)[:,1]
+#    model = naive_bayes.GaussianNB()
+#    model = linear_model.LogisticRegression()
+#    model = ensemble.GradientBoostingClassifier(max_depth=9, verbose=10)
+    model = ensemble.ExtraTreesClassifier(n_estimators=400, random_state=0,
+        n_jobs=7, verbose=1, max_features=30, max_depth=12, 
+        min_samples_leaf=100, min_samples_split=100)
+#    model = ensemble.RandomForestClassifier(n_estimators=500, n_jobs=8, 
+#        verbose=10)
+#    model.fit(x_train, y_train)
+#    y_pred = model.predict(x_test)
+#    y_pred_proba = model.predict_proba(x_test)[:,1]
+#    save_submission(eventid_test, y_pred_proba, y_pred, 
+#        '../RandomForestClassifier_submission.csv')
+    N = 15
+    models = [copy.deepcopy(model) for i in range(N)]
+    np.random.seed(0)
+    rsv = np.random.randint(10000000, size=(N,))
+    model_g = linear_model.LinearRegression(fit_intercept=False, 
+        normalize=False, copy_X=True)
+    model_g = None
+    y_pred_proba, scores, x_test_pred, x_train_pred = \
+        model_ensemble_cv(models, x_train, y_train, x_test, cv=3, 
+        random_state=rsv, model_g=model_g)
+    y_pred = (y_pred_proba>0.5).astype(int)
     save_submission(eventid_test, y_pred_proba, y_pred, 
-        '../RandomForestClassifier_submission.csv')
+        '../EnsembleCVEXT_submission.csv')
