ead9c994306e819d28a7751be11ac05c50400746
Li Yan <li.yan.ly414@gmail.com>
2016-10-12 23:01:08 +0200

    search using adaboost

--- higgs1.py
+++ higgs1.py
@@ -7,6 +7,7 @@ Created on Wed Oct 12 21:05:41 2016
 
 from startKit2 import *
 import matplotlib.pyplot as plt
+import xgboost as xgb
 
 if __name__ == '__main__':
     np.random.seed(0)
@@ -18,9 +19,10 @@ if __name__ == '__main__':
         n_jobs=7, verbose=1, max_features=30, max_depth=12, 
         min_samples_leaf=100, min_samples_split=100)
     model = ensemble.AdaBoostClassifier(n_estimators=20, learning_rate=0.75,
-        base_estimator=ExtraTreesClassifier(n_estimators=400, max_features=30, 
-        max_depth=12, min_samples_leaf=100, min_samples_split=100, verbose=1, 
-        n_jobs=7))
+        base_estimator=ensemble.ExtraTreesClassifier(n_estimators=400, 
+        max_features=30,  max_depth=12, min_samples_leaf=100, 
+        min_samples_split=100, verbose=1, n_jobs=7))
+
     
     model.fit(x_train, y_train)
     y_pred_proba = model.predict_proba(x_train)[:,1]
