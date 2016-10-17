07902811ddee76d5519c17e8674747c09a70072c
Li Yan <li.yan.ly414@gmail.com>
2016-10-17 13:24:18 +0200

    randomized search for EXT, calculating...

--- higgs7gridSearch.py
+++ higgs7gridSearch.py
@@ -32,10 +32,12 @@ if __name__ == '__main__':
     params['max_depth'] = [9, 12, 15, 18]
     params['min_samples_leaf'] = [10, 30, 60, 100]
     params['min_samples_split'] = [10, 30, 60, 100]
+    fit_params = {'sample_weight':weight.as_matrix()}
     gridSearch = grid_search.RandomizedSearchCV(model, params, n_iter=100, 
-        scoring='roc_auc', cv=3, verbose=10)
+        scoring='roc_auc', cv=3, verbose=1, fit_params=fit_params, 
+        random_state=0)
     
-    gridSearch.fit(x_train, y_train)
+    gridSearch.fit(x_train, y_train, )
     
     if not os.path.exists('gridSearch'):
         os.mkdir('gridSearch')
