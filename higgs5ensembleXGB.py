6edeffd7efc9e5cc748971616ace940b9c4ae5f9
Li Yan <li.yan.ly414@gmail.com>
2016-10-18 08:27:29 +0200

    Add new features

--- higgs5ensembleXGB.py
+++ higgs5ensembleXGB.py
@@ -13,11 +13,14 @@ Created on Thu Oct 13 09:07:18 2016
 from startKit2 import *
 import xgboost as xgb
 import matplotlib.pyplot as plt
+from add_features import add_features
 
 if __name__ == '__main__':
     np.random.seed(0)
     x_train, y_train, weight, x_test, eventid_train, eventid_test = load_data()
 #    x_train, x_test, X = data_processing(x_train, x_test)
+    x_train = add_features(x_train)
+    x_test = add_features(x_test)
     
     train_mat = xgb.DMatrix(data=x_train, label=y_train, weight=weight)
     test_mat = xgb.DMatrix(data=x_test)
@@ -32,12 +35,13 @@ if __name__ == '__main__':
     param['eta'] = 0.01
     param['max_depth'] = 9
     param['sub_sample'] = 0.9
+    param['gamma'] = 0.1
     param['eval_metric'] = 'auc'
     param['silent'] = 0
     param['nthread'] = 8
     n_rounds = 3000
-    N = 10
-    cv = 3
+    N = 12
+    cv = 5
     random_state = 0
     
     scores_auc, x_test_pred_proba, x_train_pred_proba = \
